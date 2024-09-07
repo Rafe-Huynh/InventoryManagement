3cf1cdacd898ce0b70f6bba9a978e9a182e61e00
Thai Huynh <rafehuynh0510@gmail.com>
2024-09-06 19:38:17 -0500

    darkmode, lightmode

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,17 +1,21 @@
 "use client";
 import React from "react";
 import { Button } from "./ui/button";
-import { Menu, Bell, Sun, Settings } from "lucide-react";
+import { Menu, Bell, Sun, Settings, Moon } from "lucide-react";
 import { Input } from "./ui/input";
 import Image from "next/image";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "../app/redux";
-import { setIsSidebarCollapsed } from "../state";
+import { setIsDarkMode, setIsSidebarCollapsed } from "../state";
 const Navbar = () => {
   const dispatch = useAppDispatch()
     const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
     const toggleSidebar = () => {
         dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+    }
+    const toggleDarkMode= () => {
+      dispatch(setIsDarkMode(!isDarkMode))
     }
   return (
     <div className="flex justify-between items-center w-full mb-7">
@@ -37,8 +41,11 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5 ">
           <div>
-            <Button onClick={() => {}}>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <Button onClick={toggleDarkMode}>
+              {
+                isDarkMode ? <Moon className="cursor-pointer text-gray-500" size={24} /> : <Sun className="cursor-pointer text-gray-500" size={24} />
+              }
+              
             </Button>
           </div>
           <div className="relative">
